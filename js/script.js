b62a2234ce4db38a0227f0197cd87c482ce178eb
gennadii1990@mail.ru <gennadii1990@mail.ru>
2021-08-19 00:39:58 +0300

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,10 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let accessAllowed;
+let age = prompt('Сколько вам лет?', '');
 
+if (age > 18) {
+  accessAllowed = true;
+} else {
+  accessAllowed = false;
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt("Один из последних фильмов, который вы смотрели?", ""),
-      b = prompt("На сколько оцените данный фильм?", ""),
-      c = prompt("Один из последних фильмов, который вы смотрели?", ""),
-      d = prompt("На сколько оцените данный фильм?", "");
-
-
-
-
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
-
-      console.log(personalMovieDB);
+alert(accessAllowed);
